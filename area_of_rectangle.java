bdcc826872b8e16624bd1201680bacc7d0801f96
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-20 22:07:41 +0530

    Create area_of_rectangle.java

---
+++ area_of_rectangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Area
+{
+void main()
+{
+int l,b;
+double area=0.0;
+Scanner sc=new Scanner (System.in);
+System.out.println("enter the value of l");
+l=sc.nextInt();
+System.out.println("enter the value of b");
+b=sc.nextInt();
+area=l*b;
+System.out.println("the area of a rectangle is"+ area);
+}
+}
