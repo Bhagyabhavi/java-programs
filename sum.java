c06c8e553a8491a0df3ce448b3642ae4074b7081
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-20 22:04:09 +0530

    basic java

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sum
+{
+void main()
+{
+int a,b,sum=0;
+Scanner sc=new Scanner (System.in);
+System.out.println("enter the value of a");
+a=sc.nextInt();
+System.out.println("enter the value of b");
+b=sc.nextInt();
+sum=a+b;
+System.out.println("the sum of a and b is"+ sum);
+ }
+}
