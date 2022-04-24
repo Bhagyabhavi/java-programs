943ad17c4e916e6c09a9bf0941a438a9f91862cf
Bhagyabhavi <103823947+Bhagyabhavi@users.noreply.github.com>
2022-04-24 09:36:48 +0530

    Create simple_interest.java

---
+++ simple_interest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Si
+{
+void main()
+{
+int l,b;
+double r,SI=0.0;
+Scanner sc=new Scanner (System.in);
+ System.out.println("enter the value of p");
+p=sc.nextInt();
+System.out.println("enter the value of r");
+r=sc.nextDouble();
+System.out.println("enter the value of t");
+t=sc.nextInt();
+SI=(p*r*t)/100;
+System.out.println("the simple interest is"+ SI);
+}
+}
